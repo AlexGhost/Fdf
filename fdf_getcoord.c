ab650d6894d4667e04c5f58afe6c4e8404189cd7
acourtin <acourtin@student.42.fr>
2017-12-16 14:02:00 +0100

    norm

--- fdf_getcoord.c
+++ fdf_getcoord.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/15 15:23:45 by acourtin          #+#    #+#             */
-/*   Updated: 2017/12/15 17:56:43 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/12/16 14:00:16 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,14 +53,5 @@ void			fdf_getcoord_ver(t_coord *c, int **tab, int i[2], int w)
 	c->y1 = ((i[1] * (w * SIZE)) - (tab[i[1]][i[0]] * ELEV)) + OFFSET;
 	c->x2 = (i[0] * (w * SIZE)) + OFFSET;
 	c->y2 = (((i[1] + 1) * (w * SIZE)) - (tab[i[1] + 1][i[0]] * ELEV)) + OFFSET;
-	convert_iso(c);
-}
-
-void			fdf_getcoord_lasthor(t_coord *c, int **tab, int i[2], int w, int h)
-{
-	c->x1 = (i[0] * (w * SIZE)) + OFFSET;
-	c->y1 = (((h - 1) * (w * SIZE)) - (tab[h - 1][i[0]] * ELEV)) + OFFSET;
-	c->x2 = ((i[0] + 1) * (w * SIZE)) + OFFSET;
-	c->y2 = (((h - 1) * (w * SIZE)) - (tab[h - 1][i[0] + 1] * ELEV)) + OFFSET;
 	convert_iso(c);
 }
